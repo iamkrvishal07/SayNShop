b5f11f2eb30bf7a1e3cf526d7aef0fc1f019ee7a
Vishal Kumar <141834992+iamkrvishal07@users.noreply.github.com>
2025-07-19 19:30:57 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -63,16 +63,21 @@ export const login = async (req,res) => {
     }
     
 }
-export const logOut = async (req,res) => {
-try {
-    res.clearCookie("token")
-    return res.status(200).json({message:"logOut successful"})
-} catch (error) {
-    console.log("logOut error")
-    return res.status(500).json({message:`LogOut error ${error}`})
+
+export const logOut = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none"
+    });
+    return res.status(200).json({ message: "logOut successful" });
+  } catch (error) {
+    console.log("logOut error", error);
+    return res.status(500).json({ message: `LogOut error ${error}` });
+  }
 }
-    
-}
+
 
 
 export const googleLogin = async (req,res) => {
